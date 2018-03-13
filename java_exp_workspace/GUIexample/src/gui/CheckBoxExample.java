4846f52ad565c9aa3ff290583accddf0848d678f
Brad Stiff <bstiff@iastate.edu>
2018-03-13 16:00:52 -0500

    checkBoxExample class

---
+++ java_exp_workspace/GUIexample/src/gui/CheckBoxExample.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class CheckBoxExample extends JFrame implements ActionListener
+{
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		
+		
+	}
+
+}
