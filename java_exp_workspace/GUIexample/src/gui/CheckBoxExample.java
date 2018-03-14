cb0a9d7127624e04c2ad0208549912c0d32b808c
Brad Stiff <bstiff@iastate.edu>
2018-03-13 23:17:26 -0500

    added radio buttons

--- java_exp_workspace/GUIexample/src/gui/CheckBoxExample.java
+++ java_exp_workspace/GUIexample/src/gui/CheckBoxExample.java
@@ -10,6 +10,7 @@ public class CheckBoxExample extends JFrame implements ActionListener
 	//variables
 	JLabel l;
 	JCheckBox cb1, cb2, cb3;
+	JRadioButton rb1, rb2, rb3;
 	JButton b;
 
 	CheckBoxExample()
@@ -22,13 +23,25 @@ public class CheckBoxExample extends JFrame implements ActionListener
 		cb1 = new JCheckBox("Pizza 2.00");
 		cb1.setBounds(100,100,150,20);
 		
+		//pizza radio button
+		rb1 = new JRadioButton();
+		rb1.setBounds(50,100,50,20);
+		
 		//burger check box
 		cb2 = new JCheckBox("Burger 3.50");
 		cb2.setBounds(100,150,150,20);
 		
+		//burger radio button
+		rb2 = new JRadioButton();
+		rb2.setBounds(50,150,50,20);
+		
 		//tea check box
 		cb3 = new JCheckBox("Tea 1.50");
 		cb3.setBounds(100,200,150,20);
+		
+		//tea radio button
+		rb3 = new JRadioButton();
+		rb3.setBounds(50,200,50,20);
 		
 		//order button
 		b = new JButton("Order");
@@ -38,8 +51,11 @@ public class CheckBoxExample extends JFrame implements ActionListener
 		//add components
 		add(l);
 		add(cb1);
+		add(rb1);
 		add(cb2);
+		add(rb2);
 		add(cb3);
+		add(rb3);
 		add(b);
 		
 		//jframe layout
@@ -57,12 +73,28 @@ public class CheckBoxExample extends JFrame implements ActionListener
 		{
 			float amount = 0;
 			String order = "";
-			if(cb1.isSelected()) {amount += 2.00; order += "Pizza: 2.00\n"; }
-			if(cb2.isSelected()) {amount += 3.50; order += "Burger: 3.50\n"; }
-			if(cb3.isSelected()) {amount += 1.50; order += "Tea: 1.50\n"; }
+			
+			//pizza
+			if(cb1.isSelected() || rb1.isSelected())
+			{
+				if(cb1.isSelected() && rb1.isSelected()) { amount += 2*(2.00); order += "2 x Pizza: 4.00\n"; }
+				else { amount += 2.00; order += "Pizza: 2.00\n"; }
+			}
+			//burger
+			if(cb2.isSelected() || rb2.isSelected())
+			{
+				if(cb2.isSelected() && rb2.isSelected()) { amount += 2*(3.50); order += "2 x Burger: 7.00\n"; }
+				else {amount += 3.50; order += "Burger: 3.50\n"; }
+			}
+			//tea
+			if(cb3.isSelected() || rb3.isSelected())
+			{
+				if(cb3.isSelected() && rb3.isSelected()) { amount += 2*(1.50); order += "2 x Tea: 1.50\n"; }
+				else { amount += 1.50; order += "Tea: 1.50\n"; }
+			}
 			
 			order += "--------------------\n";
-			JOptionPane.showMessageDialog(this, order + "Total: " + amount);
+			JOptionPane.showMessageDialog(this, order + "Total: " + String.format("%.02f", amount)); //show order in 2 decimal places
 		}
 		
 	}
