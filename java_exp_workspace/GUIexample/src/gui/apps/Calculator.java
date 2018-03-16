b3508c523db6263894d977a28ab4f47c37741cdb
Brad Stiff <bstiff@iastate.edu>
2018-03-16 18:06:47 -0500

    added calculator class

---
+++ java_exp_workspace/GUIexample/src/gui/apps/Calculator.java
@@ -0,0 +1,17 @@
+package gui.apps;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator extends JFrame
+{
+	private static final long serialVersionUID = 1L;
+
+	Calculator()
+	{
+		
+	}
+	
+	public static void main(String[] args) { new Calculator(); }
+}
