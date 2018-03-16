d9c279da1642f39b0ee8775b22c7588db7344fbb
Brad Stiff <bstiff@iastate.edu>
2018-03-16 17:59:07 -0500

    added notepad class

---
+++ java_exp_workspace/GUIexample/src/gui/apps/Notepad.java
@@ -0,0 +1,28 @@
+package gui.apps;
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Notepad extends JFrame implements ActionListener
+{
+	Notepad()
+	{
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		
+		
+	}
+	public static void main(String[] args) { new Notepad(); }
+}
+
+class FileOperation
+{
+	
+}
